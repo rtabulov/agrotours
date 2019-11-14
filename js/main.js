11fa448a20be607c4cb58831306ed88c050737c0
rtabulov <rasul.tabulov@gmail.com>
2019-11-14 15:01:42 +0600

    q

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+  M.AutoInit();
+  var elems = document.querySelectorAll(".carousel");
+  var instances = M.Carousel.init(elems, {
+    fullWidth: true,
+    indicators: true
+  });
+
+  var instance = M.Carousel.getInstance(document.querySelector(".carousel"));
+
+  console.log(instance);
+  var intervalID = setInterval(function() {
+    instance.next();
+  }, 7000);
+});
